ff886322edc5572a6a17b476a4a40287b3159fc8
Ciro Duran Santillli <ciro.santilli@gmail.com>
2013-05-05 22:22:50 +0200

    stopped doing git add automatically, better user messages, check for existing files

--- install.sh
+++ install.sh
@@ -7,7 +7,18 @@ set -e # stop execution if one command returns != 0
 
 BNAME="$( basename "$( pwd )" )"
 cd ..
-for b in makefile .gitignore shared.sty shared-presentation.sty; do
-    ln -s "$BNAME"/"$b" "$b"
-    git add "$b"
+
+FS=( makefile .gitignore shared.sty shared-presentation.sty )
+for F in "${FS[@]}"; do
+    if [ -e "$F" ]; then
+        echo "FILE ALREADY EXISTS. INSTALLATION ABORTED: $f"
+        exit 1
+    fi
 done
+
+for F in "${FS[@]}"; do
+    ln -s "$BNAME"/"$F" "$F"
+done
+
+echo 'INSTALLATION FINISHED. CONSIDER ADDING GENERATED FILES TO PROJECT WITH: `git add`'
+exit 0
